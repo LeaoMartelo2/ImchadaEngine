269ecf01d6527859f9f90489763cd15435e2ab18
LeaoMartelo <leaomartelo@gmail.com>
2024-05-25 00:07:16 -0300

    early screne manipulation stuff

--- engine/engine.h
+++ engine/engine.h
@@ -1,9 +1,10 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include "cstring"
+#include <cstring>
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 
 #define MAX_LOGGING_LEVEL 2
 #define LOG_ERROR 1
@@ -18,8 +19,6 @@ private:
 
   void process_arguments(int argc, char *argv[]);
 
-  char scene[30][80];
-
 public:
   Instance(int argc, char *argv[]);
 
@@ -30,9 +29,26 @@ public:
   bool get_debug_state();
   bool get_verbose_state();
   int get_logging_level();
-};
 
-// logging
+  // scene stuff
+  /*  @TODO  */
+
+private:
+  char m_scene_buffer[30][80];
+
+  std::vector<char[30][80]> scenes;
+
+public:
+  char curret_scene[30][80];
+
+  int add_scene(char new_scene[][80]); // you add a screne and it returns you
+                                       // with the scene id
+
+  int load_scene(int scene_id); // loads the scene for the given scene id
+
+  void reload_current(); // reloads the current scene
+
+}; // Instance
 
 void imchada_log(std::string log_message, int log_level);
 
