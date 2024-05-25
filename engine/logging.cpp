df09c53f0fa52330a75514ee655e7bda44a18ee5
LeaoMartelo <leaomartelo@gmail.com>
2024-05-24 23:42:23 -0300

    more inconsistencies in the engine

--- engine/logging.cpp
+++ engine/logging.cpp
@@ -6,12 +6,12 @@ const char *logLevel_0 = "[ImchadaEngine]: ";
 const char *logLevel_1 = "[ImchadaEngine] [ERROR]: ";
 const char *logLevel_2 = "[ImchadaEngine] [WARN]: ";
 
-void imchada_log(std::string log_message, unsigned short log_level) {
+void imchada_log(std::string log_message, int log_level) {
 
   // print_header(), <message>
 }
 
-void imchada_log(char *message, unsigned short log_level) {
+void imchada_log(char *message, int log_level) {
 
   // print_header(), <message>
 }
