7a134ff761aa962ac30829dc752b15d2f7750037
LeaoMartelo <leaomartelo@gmail.com>
2024-07-21 14:10:41 -0300

    scene cleanup

--- engine/engine.h
+++ engine/engine.h
@@ -16,7 +16,6 @@ class Instance {
 private:
   bool m_isDebug;
   bool m_isVerbose;
-  //  int m_logging_level;
   bool m_Logging;
 
   void process_arguments(int argc, char *argv[]);
@@ -26,7 +25,6 @@ public:
 
   void set_debug(bool state);
   void set_verbose(bool state);
-  // void set_logging_level(int level);
   void set_logging(bool state);
 
   bool get_debug_state();
@@ -44,22 +42,7 @@ public:
   /*  @TODO  */
 
 private:
-  char m_scene_buffer[30][80];
-
-  std::vector<char[30][80]> scenes;
-
 public:
-  char current_scene[30][80];
-
-  int add_scene(char new_scene[][80]); // you add a screne and it returns you
-                                       // with the scene id
-
-  int load_scene(int scene_id); // loads the scene for the given scene id
-
-  void setup_scenes(); // map display, hud, debug info, logs and etc
-
-  void reload_current(); // reloads the current scene
-
 }; // Instance
 
 } // namespace Imchada
--- engine/engine_state.cpp
+++ engine/engine_state.cpp
@@ -10,11 +10,4 @@ bool Instance::get_verbose_state() { return m_isVerbose; }
 void Instance::set_logging(bool state) { m_Logging = state; }
 bool Instance::get_logging_state() { return m_Logging; }
 
-/*
-void Imchada::Instance::set_logging_level(int level) {
-  m_logging_level = level;
-}
-int Imchada::Instance::get_logging_level() { return m_logging_level; }
-
-*/
 } // namespace Imchada
